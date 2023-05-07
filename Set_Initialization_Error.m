c9d3f9eb5f156903856ed419a8eb4e44bb870437
saeed mozafari <sa_mozafari@yahoo.com>
2023-05-08 02:14:14 +0430

    Start
    main files added

---
+++ Set_Initialization_Error.m
@@ -0,0 +1,23 @@
+
+% Constants
+deg_to_rad = 0.01745329252;
+% rad_to_deg = 1/deg_to_rad;
+% micro_g_to_meters_per_second_squared = 9.80665E-6;
+
+% Position initialization error (m; N,E,D)
+initialization_errors_Slave.delta_r_eb_n = [4;2;0];
+% Velocity initialization error (m/s; N,E,D)
+% initialization_errors_Slave.delta_v_eb_n = [0.05;-0.05;0.1];
+initialization_errors_Slave.delta_v_eb_n = [5;5;5];
+% Attitude initialization error (deg, converted to rad; @N,E,D)
+% initialization_errors.delta_eul_nb_n = [-0.05;0.04;1]*deg_to_rad; % rad
+initialization_errors_Slave.delta_eul_nb_n = [5;5;5]*deg_to_rad; % rad
+
+
+% Position initialization error (m; N,E,D)
+initialization_errors_Master.delta_r_eb_n = [0;0;0];
+% Velocity initialization error (m/s; N,E,D)
+initialization_errors_Master.delta_v_eb_n = [0;0;0];
+% Attitude initialization error (deg, converted to rad; @N,E,D)
+% initialization_errors.delta_eul_nb_n = [-0.05;0.04;1]*deg_to_rad; % rad
+initialization_errors_Master.delta_eul_nb_n = [0;0;0]*deg_to_rad; % rad
